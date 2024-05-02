23cd1a34122b9a31829a6ca5a6155412d0bb1889
table-harmony <lironkaner2007@gmail.com>
2024-05-03 00:16:45 +0300

    More projects

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <section className="flex flex-col gap-4">
-      <p className="font-semibold text-lg">Harmonial Projects</p>
-      <div className="flex flex-col gap-4">
+    <main className="flex flex-col gap-4">
+      <section className="flex flex-col gap-4">
+        <p className="font-semibold text-lg">Harmonial Projects</p>
         <div className="flex flex-col gap-2">
           <Link
             href="https://devharmony.vercel.app"
@@ -44,7 +44,65 @@ export default function HomePage() {
             ASP.NET web application project.
           </span>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="flex flex-col gap-4">
+        <p className="font-semibold text-lg">Adventurous Games</p>
+        <div className="flex flex-col gap-2">
+          <Link
+            href="https://github.com/table-harmony/breakoutadventure"
+            target="_blank"
+            rel="noreferrer"
+            className="underline underline-offset-4"
+          >
+            BreakoutAdventure
+          </Link>
+          <span className="text-muted-foreground text-sm">
+            My first game using pygame.
+          </span>
+        </div>
+        <div className="flex flex-col gap-2">
+          <Link
+            href="https://github.com/table-harmony/chessadventure"
+            target="_blank"
+            rel="noreferrer"
+            className="underline underline-offset-4"
+          >
+            ChessAdventure
+          </Link>
+          <span className="text-muted-foreground text-sm">
+            Very poorly designed Chess application.
+          </span>
+        </div>
+      </section>
+      <section className="flex flex-col gap-4">
+        <p className="font-semibold text-lg">Others</p>
+        <div className="flex flex-col gap-2">
+          <Link
+            href="https://github.com/table-harmony/nyan-cat"
+            target="_blank"
+            rel="noreferrer"
+            className="underline underline-offset-4"
+          >
+            NyanCat
+          </Link>
+          <span className="text-muted-foreground text-sm">
+            Made out of pure CSS and HTML.
+          </span>
+        </div>
+        <div className="flex flex-col gap-2">
+          <Link
+            href="https://github.com/table-harmony/cards"
+            target="_blank"
+            rel="noreferrer"
+            className="underline underline-offset-4"
+          >
+            Cards
+          </Link>
+          <span className="text-muted-foreground text-sm">
+            Made of HTML, CSS and JS. Heavily inspired from Hyperplexed.
+          </span>
+        </div>
+      </section>
+    </main>
   );
 }
