790b0659a1a78c22ca848188e8a7e2660001751c
table-harmony <lironkaner2007@gmail.com>
2024-05-02 15:31:31 +0300

    shadcn-ui

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { SunIcon, MoonIcon } from "lucide-react";
+
+import { Theme } from "@/lib/utils";
+import { useTheme } from "next-themes";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
+export function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" aria-label="theme-toggle">
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme(Theme.LIGHT)}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme(Theme.DARK)}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme(Theme.SYSTEM)}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "tableharmony",
+  url: "https://tableharmony.io",
+  author: "liron kaner",
+  description: "portfolio",
+  links: {
+    github: "https://github.com/table-harmony/tableharmony",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
